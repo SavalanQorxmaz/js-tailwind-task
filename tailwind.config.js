e602497d57b75a08be5320810f342df6e8cdd844
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2023-08-15 13:46:01 +0400

    initial configuration

---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      xs: { max: '576px' },
+      sm: { min: '577px', max: '768px' },
+      md: { min: '769px', max: '992px' },
+      lg: { min: '993px', max: '1200px' },
+      xl: { min: '1201px' }
+
+    },
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          margin: 'auto',
+          '@screen xs': {
+            maxWidth: '100%',
+          },
+          '@screen sm': {
+            maxWidth: '576px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '992px',
+          },
+          '@screen xl': {
+            maxWidth: '1200px',
+          },
+        },
+        
+        '.width-xs': {
+          margin: 'auto',
+          width: '576px'
+        }
+      }
+      
+      
+      )
+    }
+  ]
+}
